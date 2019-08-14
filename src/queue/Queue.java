f8ce7b293d04eb5095ee8188d67e218d29fff577
Administrator <Administrator@WIN-VULFF7CJ3JQ>
2019-08-14 15:20:25 +0800

    queue

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -1,10 +1,28 @@
 package queue;
 
+
 public class Queue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("12");
+		int[] qq={6,3,1,7,5,8,9,2,4};		
+		Queue1(qq);
+		System.out.println();
+		System.out.println("615947283");
+	}
+	public static void Queue1(int[] arr){		
+		int[] queue=new int[64];
+		for(int i=0;i<arr.length;i++){
+			queue[i]=arr[i];
+		}
+		int head=0;
+		int tail=9;
+		while(head<tail){
+			System.out.print(queue[head]);
+			head++;
+			queue[tail]=queue[head];
+			tail++;
+			head++;
+		}				
 	}
 
 }
